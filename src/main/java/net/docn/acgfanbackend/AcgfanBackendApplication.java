3121e21821b3fef1bbad4f7467825e8dc9b7bb54
zzznext <zzznext@163.com>
2024-05-15 15:20:57 +0800

    项目更新

---
+++ src/main/java/net/docn/acgfanbackend/AcgfanBackendApplication.java
@@ -0,0 +1,13 @@
+package net.docn.acgfanbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcgfanBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcgfanBackendApplication.class, args);
+    }
+
+}
